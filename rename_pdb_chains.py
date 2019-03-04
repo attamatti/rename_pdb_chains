4978787466c3840b4f763769b38858ff3dfea5e9
Matt Iadanza <m.iadanza@leeds.ac.uk>
2019-03-04 10:31:47 +0000

    initial

---
+++ rename_pdb_chains.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+
+try:
+    pdbfile = sys.argv[1]
+    chain_names = {}
+    for x in sys.argv[2:]:
+        chain_names[x.split(',')[0]] = x.split(',')[1]
+except:
+    sys.exit('USAGE: rename_pdb_chain.py <pdbfile> <old 1>,<new 1> <old 2>,<new 2> ...<old n>,<new n>')
+
+pdbdata = open(pdbfile,'r').readlines()
+
+output = open('{0}_rechained.pdb'.format(pdbfile.split('.')[0]),'w')
+for line in pdbdata:
+        if line[0:4] =='ATOM':
+            chain = line[21]
+            print(line.replace('\n',''))
+            newline = '{0}{1}{2}'.format(line[0:21],chain_names[chain],line[22:])
+            print(newline)
+            output.write(newline)
